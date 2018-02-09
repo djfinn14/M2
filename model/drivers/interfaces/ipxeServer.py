6183ec31b5dd6c32194e68d2f6113a2b1729f571
nasibehteimouri <nasibehteimouri@gmail.com>
2018-02-08 22:11:26 -0500

    pep8 checked

--- model/drivers/interfaces/ipxeServer.py
+++ model/drivers/interfaces/ipxeServer.py
@@ -3,22 +3,14 @@ from abc import ABCMeta, abstractmethod
 
 class ipxeServer:
 
-	__metaclass__ = ABCMeta
+    __metaclass__ = ABCMeta
 
-    
-	@abstractmethod
-	def ipxeServer(self, conf_file):
-	"""Set up ipxe"""
-	pass
+    @abstractmethod
+    def ipxeServer(self, conf_file):
+        """Set up ipxe"""
+    pass
 
-
-
-	@abstractmethod
-	def generate_ipxe_file(self, node_name, target_name):
-	"""Generate ipxe file"""
-	pass
-
-	
-
-
-
+    @abstractmethod
+    def generate_ipxe_file(self, node_name, target_name):
+        """Generate ipxe file"""
+    pass

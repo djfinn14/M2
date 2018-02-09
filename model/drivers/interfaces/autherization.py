a86d5cba690e71f6d4545f6b8045ab4f1cc8b3bd
nasibehteimouri <nasibehteimouri@gmail.com>
2018-02-08 22:15:21 -0500

    pep8 checked

--- model/drivers/interfaces/autherization.py
+++ model/drivers/interfaces/autherization.py
@@ -1,22 +1,21 @@
 from abc import ABCMeta, abstractmethod
 
+
 class autherization:
 
-	__metaclass__ = ABCMeta
+    __metaclass__ = ABCMeta
 
+    @abstractmethod
+    def autherize_access_node(self, nodeId):
+        """Autherize entity's access to a specific node"""
+    pass
 
-	@abstractmethod
-	def autherize_access_node(self, nodeId):
-	"""Autherize entity's access to a specific node"""
-	pass
+    @abstractmethod
+    def autherize_access_image(self, imageId):
+        """Autherize entity's access to a specific image"""
+    pass
 
-	@abstractmethod
-	def autherize_access_image(self, imageId):
-	"""Autherize entity's access to a specific image"""
-	pass
-
-	@abstractmethod
-	def autherize_access_tag(self, tagId):
-	"""Autherize entity's access to a specific tag"""
-	pass
-
+    @abstractmethod
+    def autherize_access_tag(self, tagId):
+        """Autherize entity's access to a specific tag"""
+    pass

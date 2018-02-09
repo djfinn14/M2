d5e5fc670c2c51ce3a4432674c40c9a21d279013
nasibehteimouri <nasibehteimouri@gmail.com>
2018-02-08 22:17:45 -0500

    pep8 checked

--- model/drivers/interfaces/authentication.py
+++ model/drivers/interfaces/authentication.py
@@ -1,19 +1,16 @@
 from abc import ABCMeta, abstractmethod
 
+
 class authentication:
 
-	__metaclass__ = ABCMeta
+    __metaclass__ = ABCMeta
 
+    @abstractmethod
+    def isAdmin(self):
+        """verify admin's authority"""
+    pass
 
-	@abstractmethod
-	def isAdmin(self):
-	"""verify admin's authority"""
-	pass
-
-	@abstractmethod
-	def isLegit(self, entityId):
-	"""verify user's authority"""
-	pass
-	
-
-
+    @abstractmethod
+    def isLegit(self, entityId):
+        """verify user's authority"""
+    pass

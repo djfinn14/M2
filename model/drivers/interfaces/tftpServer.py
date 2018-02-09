68e46ed4341cebafd766793d340eb252bd430570
nasibehteimouri <nasibehteimouri@gmail.com>
2018-02-08 21:57:03 -0500

    pep8 checked!

--- model/drivers/interfaces/tftpServer.py
+++ model/drivers/interfaces/tftpServer.py
@@ -2,21 +2,14 @@ from abc import ABCMeta, abstractmethod
 
 
 class tftpServer:
+    __metaclass__ = ABCMeta
 
-	__metaclass__ = ABCMeta
+    @abstractmethod
+    def tftpServer(self, conf_file):
+        """Set up TFTP server"""
+    pass
 
-    
-	@abstractmethod
-	def tftpServer(self, conf_file):
-	"""Set up TFTP server"""
-	pass
-
-
-	@abstractmethod
-	def generate_mac_addr_file(self, img_name, node_name, mac_addr):
-	"""Generate mac address file"""
-	pass
-	
-
-
-
+    @abstractmethod
+    def generate_mac_addr_file(self, img_name, node_name, mac_addr):
+        """Generate mac address file"""
+    pass
